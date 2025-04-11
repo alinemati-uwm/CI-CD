82ce4b41fa9f66483f5095164bcd9cd1303fd598
rezashakeri45 <nemati@uwm.edu>
2025-04-11 03:04:18 -0500

    python3

--- Deploy/deployer.sh
+++ Deploy/deployer.sh
@@ -1,2 +1,2 @@
 ansible-playbook ./mainplay.yml --limit prod
-python3 -m pytest ./Deploy/validateospf.py --disable-warnings -s --verbose
+python -m pytest ./Deploy/validateospf.py --disable-warnings -s --verbose
